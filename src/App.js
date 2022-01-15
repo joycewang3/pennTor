ed664b57cfe2daccb00fcf19fdfa49d22be7e81b
Joyce An-Jie Wang <joycewang821993@gmail.com>
2022-01-14 20:02:46 -0500

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; //create new .css file e.g. header.css and import in header.js
+import Header from './header';
 
 function App() {
+  
+  const greet = () =>{
+    console.log("button clicked");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <button onClick={greet} id="button" className="allbuttons">Mentor</button>
+      <form>
+        <label for="firstname">
+          First Name
+        </label>
+        <input type="text" name="firstname" required>
+          
+        </input>
+      </form>
+
+      <div className="box">
+
+      </div>
+
     </div>
   );
 }
