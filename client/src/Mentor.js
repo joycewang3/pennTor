5bea7d120a854723ed7450bd76b554015f94162f
Joyce An-Jie Wang <joycewang821993@gmail.com>
2022-01-14 21:18:48 -0500

    Add mentor form

---
+++ client/src/Mentor.js
@@ -0,0 +1,19 @@
+import './Mentor.css';
+import React from "react"
+
+
+function Mentor() {
+    return (
+        <div>
+            <header className="pennTor">pennTor</header>
+            <a
+            className='findMentee'>
+                Answer the questions below to help us find your perfect mentee match!
+            </a>
+            <div className="q1box">
+
+            </div>
+        </div>
+
+    );
+}
