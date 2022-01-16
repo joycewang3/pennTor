9c7f31101de0bde23b8d7998c07307e271ca68b8
Rita Sabri <rsabri@seas.upenn.edu>
2022-01-15 21:40:36 -0500

    okkk
    kkkd

--- client/src/components/Year.js
+++ client/src/components/Year.js
@@ -1,44 +1,33 @@
+
 import  '../style/Year.css'
 
 const Year = ({ name, signUp, setMenteeForm, setMentorForm, year }) => {
     const selectMentor = () => {
         setMentorForm(true);
-        setMenteeForm(false);
-    }
-
-    const selectMentee = () => {
-        setMenteeForm(true);
-        setMentorForm(false);
-    }
-
-    return (
-        <div>
-            {
-                signUp && !year &&
-                <div className="year">
-                    <h2> Hi, {name}! </h2>
-                    <h3> Are you signing up to be a mentor or mentee? </h3>
-
-                   <div className="box1">
-                       <button className="b1" onClick={selectMentor}> Mentor </button> 
-                   </div>
-                   <br></br>
-                   
-                   <h4> I’m a 2nd year MCIT student </h4>                       
-                        <div className="box2">
-                        <button className ="b2" onClick={selectMentee}> Mentee </button>
-                   <div className="box2">
-                    </div>
-                    <br></br>
-                   <h5> I’m a 1nd year MCIT student </h5>
-                
-               </div>
-                </div>
-            }
 
 
+        <div className="year">
+        <h2> Hi, {name}! </h2>
+        <h3> Are you signing up to be a mentor or mentee? </h3>
+        <button onClick={selectMentor}> Mentor </button>
+        <button onClick={selectMentee}> Mentee </button>
+
+       <div className="box1">
+           <button className="b1" onClick={selectMentor}> Mentor </button> 
+       </div>
+       <br></br>
+       
+       <h4> I’m a 2nd year MCIT student </h4>                       
+            <div className="box2">
+            <button className ="b2" onClick={selectMentee}> Mentee </button>
+       <div className="box2">
         </div>
-    )
+        <br></br>
+       <h5> I’m a 1nd year MCIT student </h5>
+   </div>
+    </div>
+    }
 }
+
 
 export default Year;
