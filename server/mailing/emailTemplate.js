21d5974856217ff3edb6de9ace7288dd3d9a03d9
JengRuWu <48342041+JengRuWu@users.noreply.github.com>
2022-01-17 14:17:13 -0500

    formatting

--- server/mailing/emailTemplate.js
+++ server/mailing/emailTemplate.js
@@ -7,13 +7,13 @@ module.exports = match => {
 					<p>Below are the details of your match</p>
                     <p>
                         <div>Mentor</div>
-                        <div>${match.mentorfirstname}" "${match.mentorlastname}</div>
+                        <div>${match.mentorfirstname} ${match.mentorlastname}</div>
                         <div>contact: ${match.mentoremail}</div>
                         <div>Message to my mentee: ${match.mentormessage}</div>
                     </p>
                     <p>
                         <div>Mentee</div>
-                        <div>${match.menteefirstname}" "${match.menteelastname}</div>
+                        <div>${match.menteefirstname} ${match.menteelastname}</div>
                         <div>contact: ${match.menteeemail}</div>
                         <div>Message to my mentor: ${match.menteemessage}</div>
                     </p>
